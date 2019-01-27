79276e432ec3f868fc32a9d5c3a0a6d194e78b84
Acacia Mack <acacia.mack@fundingcircle.com>
2019-01-27 21:02:03 +0000

    Update App.js Add state in order to retrieve data Make request to API Add handler to search for cars based on transmission

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import axios from 'axios';
+import Cars from './components/Cars/Cars'
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [],
+      loaded: false
+    }
+  }
+
+  async componentDidMount() {
+    const response = await axios.post("https://app.joindrover.com/api/web/vehicles?vehicle_type=Consumer");
+
+    if(response.status !== 200) return alert('Sorry, it\'s taking longer than expected to find you a car');
+
+    this.setState({
+      data: response.data.data,
+      loaded: true
+    });
+  }
+
+  searchGearHandler = (event,index) => {
+    const cars = this.state.data
+    const { search } = this.state;
+    let gear;
+
+    this.setState({search: event.target.value});
+
+    cars.map((car) => {
+      gear = car.transmission;
+      return gear;
+    });
+
+    if(search !== '' && gear.indexOf( search === -1 )) {
+      //console show's what's being searched
+      console.log(search);
+      return;
+    }
+
+    if(gear.indexOf(search > 0)){
+      //console show's what's being searched
+      console.log(search);
+      return
+    }
+
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input placeholder="Transmission Type" type="text" onChange={this.searchGearHandler} />
+        <Cars cars={this.state.data}/>
       </div>
     );
   }
